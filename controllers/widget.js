cc12db0af19cc4eab4b36d0e65891ada4e319e7b
Timan Rebel <timan@rebelic.nl>
2014-05-16 09:49:13 +0200

    Prevent multiple clicks firing

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,4 +1,8 @@
+var moment = require('alloy/moment');
+
 _.extend(this, {
+	delay: 2000,
+
 	construct: function(config) {
 		$.setArgs(config);
 	},
@@ -58,10 +62,26 @@ _.extend(this, {
 });
 
 /**
- * Handle click on button and pass them through to Widget
- *
- * @param {Object} evt Event details
+ * @parameter {Number} _lastClickMoment The last time this button was clicked
+ * @private
+ */
+var _lastClickMoment;
+
+/**
+ * Handle clicks on button and pass them through to Widget
+ * @param {Object} evt Event data that is fired
+ * @private
  */
 function onClick(evt) {
+	// If last click has been less than 2 seconds ago, ignore
+	if(_lastClickMoment && _lastClickMoment.isAfter(moment().subtract($.delay, 'milliseconds'))) {
+		Ti.API.error('Can\'t click within ' + $.delay + ' msec!');
+		return;
+	}
+
+	// Pass the click through to the widget
 	$.trigger('click', evt);
+
+	// Save last click timestamp
+	_lastClickMoment = moment();
 }
